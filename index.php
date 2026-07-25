b0b5fdb9aba7989a36422fe0304d95247720091e
juan.moradel <juan.moradel@est.ulsa.edu.ni>
2026-07-25 15:43:02 -0600

    pase de html a php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
+<?php 
+    $saludo="hola mundo";
+?>
 <!DOCTYPE html>
-
 <html>
     <head>
         <title>mi primera web de juan</title>
     </head>
     <body>
         <h1>!hola mundo!</h1>
+        <h2><?php echo $saludo; ?></h2>
         <p>este es mi  primera pagina web. Hola soy juan</p>
     </body>
 </html>
