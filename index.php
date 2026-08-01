d514b663114d1ce48eaa9e579c71c165d9e24534
juan.moradel <juan.moradel@est.ulsa.edu.ni>
2026-08-01 14:28:33 -0600

    codigo php sobre inventario

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
-<?php
-    $edades=[1 ,2,3,4,5,20,100,1000,18,30,40,60,90,234];
+<?php    
+$inventario = [ 
+    "laptop" => 5,
+    "mouse" => 0,
+    "teclado" => 12,
+    "monitor" => 0
+];
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>mi primera web de juan</title>
-        <h1>los mejores juegos en mi opinion</h1>
-    </head>
+    <head><title>MI TIENDA</title></head> 
     <body>
-        <h3> porton de entrada de menores </h3>
-        <?php foreach ($edades as $edad) {
-            if ($edad <= 18) { 
-                echo "<p>edad: $edad - acceso denegado</p>";
-            } else {
-                echo "<p>edad: $edad - acceso permitido</p>";
-            }
-        } ?>
+        <h2> estado de inventario </h2>
+        <ul>
+            <?php foreach ($inventario as $producto=>$cantidad){
+                if ($cantidad){
+                    echo "<li>$producto:¡disponible!(quedan $cantidad)</li>";
+                }else{
+                    echo "<li>$producto:agotado :(<li/>";
+                }
+            }?>
+        </ul>
     </body>
-</html>
+</html>        
