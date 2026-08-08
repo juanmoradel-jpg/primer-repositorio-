dc7bf8f4eb76548294077791ca6a773cc10392fa
J3remy0211 <jeremychavez0211@gmail.com>
2026-08-08 16:12:19 -0600

    sistema de cafe

--- index.php
+++ index.php
@@ -1,37 +1,58 @@
-<?php
+ <?php
+ $menu = [
+    "expresso"=> 35,
+    "americano"=>40,
+    "capuchino"=>55,
+    "latle"=>60,
 
-function verificarAcceso($edad_a_revisar){
-    if ($edad_a_revisar>=18){
-        return "acceso concedido.disfruta tu estancia.";
-    }else{
-        return"acceso denegado .vuelve cuando crezcas. ";
+ ];
+ if($_SERVER["REQUEST_METHOD"]=="POST"){   
+    $cafe_elegido=$_POST ["tipo_cafe"];
+    $cantidad = $_POST["cantidad"];
+
+    $preferencia ="con azucar";
+    if (isset ( $_POST["sin_azucar"])){
+        $preferencia = "sin azucar";
     }
-}
 
-if ($_SERVER ["REQUEST_METHOD"]=="POST"){
+    $precio_unitario = $menu[$cafe_elegido];
+    $total =$precio_unitario * $cantidad;
 
-$nombre =$_POST["nombre_usuario"];
-$edad=$_POST ["edad_usuario"];
+    echo"<div style='border : 1px solid black; padding: 15px ; margin-bottom:20px;'>";
+    echo "<h3> ticket de compra </h3>";
+    echo "<p><strong>producto:</strong>$cantidad x $cafe_elegido($preferencia)</p>";
+    echo"<p><strong>total a pagar :</strong> $$total</p>";
+    echo "</div>";
+    }
+ ?>
 
-echo "<h3>!hola,$nombre ¡el sevidor recibio su edad : $edad años </h3>";
-$resultado=verificarAcceso($edad);
-echo"<p>$resultado</p>";
-}
-?>
-
-<!DOCTYPE html>
-<html>
-    <head><title>registro</title></head> 
+ <!DOCTYPE html>
+ <html>
+    <head> <title>cafeteria</title></head>
     <body>
-        <h1> ingresa tus datos</h1>
-        <form method ="POST">
-            <label> tu nombre ;</label>
-            <input type = "text" name = "nombre_usuario" placeholder="EJ.juan">
-            <br></br>
-            <label> tu edad ;</label>
-            <input type = " number " name = "edad_usuario" placeholder ="EJ.20">
-            <br> </br>
-            <button type ="submit">enviar al servidor</button>
+        <h1>sistema de pedidos</h1>
+        <form method="POST">
+            <label>selecciona tu cafe:</label>
+
+            <select name="tipo_cafe">
+            <?php
+            foreach ($menu as $cafe => $precio) {
+                echo "<option value='$cafe'> $cafe - $$precio</option>";
+
+            }
+            ?>
+            </select>
+            <br><br>
+
+            <label>cantidad:</label>
+            <input type ="number" name="cantidad" value ='1' min ="1">
+            <br><br>
+              
+            <label>
+                <input type="checkbox" name="sin_azucar" value="si">
+                 preparar sin azucar 
+            </label>
+            <button type="submit"> procesar orden </button>
         </form>
     </body>
-</html>        
+</html>
